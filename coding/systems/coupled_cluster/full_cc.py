f286daac5462ce82a3c761524d8e32466e4acd9e
schraderSimon <simonsch@student.matnat.uio.no>
2021-11-15 15:53:12 +0100

    The bitch (me) solved EVC for CCgit add .

--- coding/systems/coupled_cluster/full_cc.py
+++ coding/systems/coupled_cluster/full_cc.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from pyscf import gto, cc,scf, ao2mo
+from pyscf import gto, cc,scf, ao2mo,fci
 import sys
-np.set_printoptions(linewidth=300,precision=2,suppress=True)
-from scipy.linalg import block_diag
+np.set_printoptions(linewidth=300,precision=4,suppress=True)
+from scipy.linalg import block_diag, eig
 from numba import jit
 from matrix_operations import *
 from helper_functions import *
@@ -186,13 +186,27 @@ def makeT2(ts,td,fs,spinints,F,Nelec,dim,Dabij,W):
 # computes CCSD energy given T1 and T2
 
 def ccsdenergy(fs,spinints,ts,td,Nelec,dim):
-	ts=ts
-	td=td
 	ECCSD = np.einsum("ia,ai->",fs[:Nelec,Nelec:dim],ts,optimize=True)
-	print(td.shape)
-	ECCSD+=0.25*np.einsum("ijab,abij->",spinints[:Nelec,:Nelec,Nelec:dim,Nelec:dim],td,optimize=True)
-	ECCSD+=0.5*np.einsum("ijab,ai,bj->",spinints[:Nelec,:Nelec,Nelec:dim,Nelec:dim],ts,ts,optimize=True)
+	ECCSD=ECCSD+0.25*np.einsum("ijab,abij->",spinints[:Nelec,:Nelec,Nelec:dim,Nelec:dim],td,optimize=True)
+	ECCSD=ECCSD+0.5*np.einsum("ijab,ai,bj->",spinints[:Nelec,:Nelec,Nelec:dim,Nelec:dim],ts,ts,optimize=True)
 	return ECCSD
+@jit(parallel=True,nopython=True)
+def make_spinints_aokjemi(dim,energy_basis_2e_mol_chem,alternating):
+    spinints_AO_kjemi=np.zeros((dim,dim,dim,dim))
+    for p in range(dim):
+    	spinP=int(2*(-(p%2)+0.5))
+    	for q in range(dim):
+    		spinQ=int(2*(-(q%2)+0.5))
+    		for r in range(dim):
+    			spinR=int(2*(-(r%2)+0.5))
+    			for s in range(dim):
+    				spinS=int(2*(-(s%2)+0.5))
+    				P=p//2
+    				Q=q//2
+    				R=r//2
+    				S=s//2
+    				spinints_AO_kjemi[p,q,r,s]=energy_basis_2e_mol_chem[P,Q,R,S]*alternating[p,q]*alternating[r,s]
+    return spinints_AO_kjemi
 
 
 
@@ -201,7 +215,7 @@ def ccsdenergy(fs,spinints,ts,td,Nelec,dim):
 basis="6-31G"
 molecule_name="HF"
 x_sol=np.linspace(1.5,4,30)
-ref_x=2/1.1386276671
+ref_x=2
 #x_sol=np.array([1.9,2.5])
 energies_ref=np.zeros(len(x_sol))
 mol=make_mol(molecule,ref_x,basis,charge=0)
@@ -213,7 +227,7 @@ mf=scf.RHF(mol)
 ESCF=mf.kernel()
 rhf_mo_ref=mf.mo_coeff
 
-xnew=2*ref_x
+xnew=1.9
 mol=make_mol(molecule,xnew,basis,charge=0)
 mf=scf.RHF(mol)
 ESCF=mf.kernel()
@@ -231,29 +245,21 @@ energy_basis_2e=mol.intor('int2e')
 
 mycc = cc.CCSD(gmf)
 mycc.kernel()
+l1_pyscf,l2_pyscf=mycc.solve_lambda()
+
 t1_pyscf=mycc.t1
 
 mo_coeff=gmf.mo_coeff
 fs=mo_coeff.T@gmf.get_fock()@mo_coeff #Fock matrix
 print(fs)
+
 energy_basis_1eMO = np.einsum('pi,pq,qj->ij', mo_coeff, energy_basis_1e, mo_coeff)
 dim=len(energy_basis_2e)*2
 dimhalf=len(energy_basis_2e)
 energy_basis_2e_mol_chem=ao2mo.get_mo_eri(energy_basis_2e,(rhf_mo,rhf_mo,rhf_mo,rhf_mo)) #Molecular orbitals in spatial basis, not spin basis. Chemists notation
-spinints_AO_kjemi=np.zeros((dim,dim,dim,dim))
-for p in range(dim):
-	spinP=int(2*(-(p%2)+0.5))
-	for q in range(dim):
-		spinQ=int(2*(-(q%2)+0.5))
-		for r in range(dim):
-			spinR=int(2*(-(r%2)+0.5))
-			for s in range(dim):
-				spinS=int(2*(-(s%2)+0.5))
-				P=p//2
-				Q=q//2
-				R=r//2
-				S=s//2
-				spinints_AO_kjemi[p,q,r,s]=energy_basis_2e_mol_chem[P,Q,R,S]*(spinP==spinQ)*(spinR==spinS)
+
+alternating=np.array([[(i+j)%2 for i in range(1,dim+1)] for j in range(dim)])
+spinints_AO_kjemi=make_spinints_aokjemi(dim,energy_basis_2e_mol_chem,alternating)
 spinints_AO_fysikk=np.transpose(spinints_AO_kjemi,(0,2,1,3))
 spinints_AO_fysikk_antisymm=spinints_AO_fysikk-np.transpose(spinints_AO_fysikk,(0,1,3,2))
 spinints=spinints_AO_fysikk_antisymm
@@ -281,8 +287,8 @@ for a in range(Nelec,dim):
 			for j in range(0,Nelec):
 				Dabij[a,b,i,j] = fs[i,i] + fs[j,j] - fs[a,a] - fs[b,b]
 
-ts = np.zeros((dim-Nelec,Nelec))
-td = np.zeros((dim-Nelec,dim-Nelec,Nelec,Nelec))
+ts = np.zeros_like(mycc.t1.T)
+td = np.zeros_like(mycc.t2.T)
 
 # Initial guess T2 --- from MP2 calculation!
 
@@ -291,11 +297,13 @@ for a in range(0,dim-Nelec):
 		for i in range(0,Nelec):
 			for j in range(0,Nelec):
 				td[a,b,i,j] += spinints[i,j,a+Nelec,b+Nelec]/(fs[i,i] + fs[j,j] - fs[a+Nelec,a+Nelec] - fs[b+Nelec,b+Nelec])
+				pass
 
 ECCSD = 0
 DECC = 1
 counter=0
-while DECC > 1e-14: # arbitrary convergence criteria
+
+while DECC > 1e-8: # arbitrary convergence criteria
 	OLDCC = ECCSD
 	F,W = updateintermediates(ts,td,Nelec,dim,fs,spinints)
 	tsnew = makeT1(ts,td,fs,spinints,F,Nelec,dim,Dai)
@@ -313,8 +321,116 @@ while DECC > 1e-14: # arbitrary convergence criteria
 	counter+=1
 print("E(corr,CCSD) = ", ECCSD)
 print("E(CCSD) = ", ECCSD + ESCF)
+
 print("Number of convergence steps: %d"%counter)
-print(ts.T)
-print(mycc.t1)
-print("Difference in T1 coefficients: %e"%(np.max(np.abs(ts.T-mycc.t1))))
-print("Difference in T2 coefficients: %e"%(np.max(np.abs(td.T-mycc.t2))))
+print("Difference in T1 coefficients compared to scipy: %e"%(np.max(np.abs(ts.T-mycc.t1))))
+print("Difference in T2 coefficients compared to scipy: %e"%(np.max(np.abs(td.T-mycc.t2))))
+
+#Now that the CC solver is in place, let's attempt to create functions for the calculation of the matrix elements as given by Ekstrøm & Hagen!!
+#I will here only continue a single CC state, but this does not really affect the mechanics.
+sample_x=[1.0,1.4,1.7,2.0,2.3,2.4,2.5]
+t1s=[]
+t2s=[]
+l1s=[]
+l2s=[]
+for x in sample_x:
+    mol=make_mol(molecule,x,basis,charge=0)
+    mf=scf.RHF(mol)
+    ESCF=mf.kernel(verbose=0)
+    rhf_mo=localize_procrustes(mol,mf.mo_coeff,mf.mo_occ,previous_mo_coeff=rhf_mo_ref)
+    #rhf_mo=mf.mo_coeff
+    mf.mo_coeff=rhf_mo
+    overlap_basis=block_diag(mol.intor("int1e_ovlp"),mol.intor("int1e_ovlp"))
+    energy_basis_1e=block_diag(mol.intor("int1e_kin")+mol.intor("int1e_nuc"),mol.intor("int1e_kin")+mol.intor("int1e_nuc"))
+    gmf=scf.addons.convert_to_ghf(mf)
+    mo_coeff=gmf.mo_coeff
+    fs=mo_coeff.T@gmf.get_fock()@mo_coeff #Fock matrix
+    energy_basis_2e=mol.intor('int2e')
+    energy_basis_2e_mol_chem=ao2mo.get_mo_eri(energy_basis_2e,(rhf_mo,rhf_mo,rhf_mo,rhf_mo)) #Molecular orbitals in spatial basis, not spin basis. Chemists notation
+    spinints_AO_kjemi=make_spinints_aokjemi(dim,energy_basis_2e_mol_chem,alternating)
+    spinints_AO_fysikk=np.transpose(spinints_AO_kjemi,(0,2,1,3))
+    spinints_AO_fysikk_antisymm=spinints_AO_fysikk-np.transpose(spinints_AO_fysikk,(0,1,3,2))
+    spinints=spinints_AO_fysikk_antisymm
+
+    mycc = cc.CCSD(gmf)
+    mycc.kernel()
+    t1_pyscf=mycc.t1
+    t2_pyscf=mycc.t2
+    t1s.append(t1_pyscf.T)
+    t2s.append(t2_pyscf.T)
+    l1s.append(l1_pyscf.T)
+    l2s.append(l2_pyscf.T)
+    #print("CC pyscf,",mycc.e_corr+ESCF)
+    #print("CC own,",ccsdenergy(fs,spinints,t1_pyscf.T,t2_pyscf.T,Nelec,dim)+ESCF)#+ESCF)
+
+    l1_pyscf,l2_pyscf=mycc.solve_lambda()
+x_alphas=np.linspace(1.5,5.0,36)
+E_CCSD=[]
+E_FCI=[]
+E_approx=[]
+print("Start EVC")
+for x_alpha in x_alphas:
+    mol=make_mol(molecule,x_alpha,basis,charge=0)
+    mf=scf.RHF(mol)
+    ESCF=mf.kernel(verbose=0)
+    rhf_mo=localize_procrustes(mol,mf.mo_coeff,mf.mo_occ,previous_mo_coeff=rhf_mo_ref)
+    #rhf_mo=mf.mo_coeff
+    mf.mo_coeff=rhf_mo
+    overlap_basis=block_diag(mol.intor("int1e_ovlp"),mol.intor("int1e_ovlp"))
+    energy_basis_1e=block_diag(mol.intor("int1e_kin")+mol.intor("int1e_nuc"),mol.intor("int1e_kin")+mol.intor("int1e_nuc"))
+    gmf=scf.addons.convert_to_ghf(mf)
+    mo_coeff=gmf.mo_coeff
+    fs=mo_coeff.T@gmf.get_fock()@mo_coeff #Fock matrix
+    energy_basis_2e=mol.intor('int2e')
+    energy_basis_2e_mol_chem=ao2mo.get_mo_eri(energy_basis_2e,(rhf_mo,rhf_mo,rhf_mo,rhf_mo)) #Molecular orbitals in spatial basis, not spin basis. Chemists notation
+    spinints_AO_kjemi=make_spinints_aokjemi(dim,energy_basis_2e_mol_chem,alternating)
+    spinints_AO_fysikk=np.transpose(spinints_AO_kjemi,(0,2,1,3))
+    spinints_AO_fysikk_antisymm=spinints_AO_fysikk-np.transpose(spinints_AO_fysikk,(0,1,3,2))
+    spinints=spinints_AO_fysikk_antisymm
+    myci = fci.FCI(mol, rhf_mo)
+    e, fcivec = myci.kernel()
+    E_FCI.append(e)
+    mycc = cc.CCSD(gmf)
+    try:
+        mycc.kernel()
+        E_CCSD.append(mycc.e_corr+ESCF)
+    except np.linalg.LinAlgError:
+        E_CCSD.append(np.nan)
+    except:
+        E_CCSD.append(np.nan)
+    H=np.zeros((len(sample_x),len(sample_x)))
+    S=np.zeros((len(sample_x),len(sample_x)))
+    Dai = np.zeros((dim,dim))
+    for a in range(Nelec,dim):
+    	for i in range(0,Nelec):
+    		Dai[a,i] = fs[i,i] - fs[a,a]
+
+    # Stanton eq (13)
+    Dabij = np.zeros((dim,dim,dim,dim))
+    for a in range(Nelec,dim):
+    	for b in range(Nelec,dim):
+    		for i in range(0,Nelec):
+    			for j in range(0,Nelec):
+    				Dabij[a,b,i,j] = fs[i,i] + fs[j,j] - fs[a,a] - fs[b,b]
+
+    for i, xi in enumerate(sample_x):
+        F,W = updateintermediates(t1s[i],t2s[i],Nelec,dim,fs,spinints)
+        t1_error=T1_eq(t1s[i],t2s[i],fs,spinints,F,Nelec,dim,Dai)
+        t2_error=T2_eq(t1s[i],t2s[i],fs,spinints,F,Nelec,dim,Dabij,W)
+        for j, xj in enumerate(sample_x):
+            X1=t1s[i]-t1s[j]
+            X2=t2s[i]-t2s[j]
+            overlap=1+np.einsum("ia,ai->",l1s[j].T,X1)+0.5*np.einsum("ijab,ai,bj->",l2s[j].T,X1,X1)+0.25*np.einsum("ijab,abij->",l2s[j].T,X2)
+            S[i,j]=overlap
+            exp_energy=ccsdenergy(fs,spinints,t1s[i],t2s[i],Nelec,dim)+ESCF
+            H[i,j]=overlap*exp_energy
+            extra=np.einsum("ia,ai->",l1s[j].T,t1_error)+np.einsum("ijab,ai,bj->",l2s[j].T,X1,t1_error)+0.25*np.einsum("ijab,abij->",l2s[j].T,t2_error)
+            H[i,j]=H[i,j]+extra
+    e,c=eig(H,b=S)
+    E_approx.append(np.min(np.real(e)))
+print("End EVC")
+plt.plot(x_alphas,E_CCSD,label="CCSD")
+plt.plot(x_alphas,E_approx,label="EVC")
+plt.plot(x_alphas,E_FCI,label="FCI")
+plt.legend()
+plt.show()
